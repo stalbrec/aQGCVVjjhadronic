c22516ad259adb0eb40672552ecf5d1ac8400a9b
Steffen <steffen.albrecht@desy.de>
2018-04-11 18:08:08 +0200

    last commit for this branch. added last version of submission-scripts. will not use them anymore

--- AQGC_SB.sh
+++ AQGC_SB.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # cd /nfs/dust/cms/user/albrechs
-/afs/desy.de/user/a/albrechs
-source setup_UHH2.sh
-cd aQGCVVjjhadronic
+# /afs/desy.de/user/a/albrechs
+# source setup_UHH2.sh
+# cd aQGCVVjjhadronic
 
 processes=(WPWP WMWM WPWM WPZ WMZ ZZ)
 
-# for i in {0..5}
-# do
-#   sframe_batch.py -s config/aQGC${processes[$i]}jjhadronic.xml
-# done
-
-sframe_main config/aQGC${processes[$i]}jjhadronic.xml
+for i in {0..5}
+do
+	qsub -cwd -N UHH2_${processes[$i]} submitSample.sh ${processes[$i]}
+	#   sframe_batch.py -s config/aQGC${processes[$i]}jjhadronic.xml
+done
