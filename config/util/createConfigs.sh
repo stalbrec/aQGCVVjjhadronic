e666ccb184e473c21cea0c1ecbfa98bb8f453a1f
Steffen <steffen.albrecht@desy.de>
2018-06-06 15:23:47 +0200

    config aufgeraeumt

--- config/util/createConfigs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-processes=(WPWP WMWM WPWM WPZ WMZ ZZ)
-
-for i in {0..5}
-do
-  cp aQGCVVjjhadronic.xml aQGC${processes[$i]}jjhadronic.xml 
-  sed -i "s|NT_VV|NT_${processes[$i]}|g" aQGC${processes[$i]}jjhadronic.xml
-  sed -i "s|_VV|_${processes[$i]}|g" aQGC${processes[$i]}jjhadronic.xml
-done
--- config/util/util.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm ntuple_set.xml
-for i in {0..99}
-do
-    echo '<In FileName="PATH_TO_NTUPLES/Ntuple_'${i}'.root" Lumi="0.0"/>' >> 'ntuple_set.xml'
-done
