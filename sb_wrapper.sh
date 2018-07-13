238e8398d91a6e8921ab265ef66f0a963a529f9e
Steffen <steffen.albrecht@desy.de>
2018-07-13 10:31:58 +0200

    added JEScale_Submit.sh

--- sb_wrapper.sh
+++ sb_wrapper.sh
@@ -3,18 +3,18 @@
 FLAG=$1
 
 # REGIONS=(PreSelection)
-REGIONS=(SignalRegion SidebandRegion)
-# REGIONS=(SignalRegion)
+# REGIONS=(SignalRegion SidebandRegion)
+REGIONS=(SignalRegion)
 # REGIONS=(SidebandRegion)
 
 for ((j=0;j<${#REGIONS[@]};j+=1))
 do
-	sframe_batch.py $FLAG config/aQGCVVjjhadronic_${REGIONS[$j]}.xml
+	# sframe_batch.py $FLAG config/aQGCVVjjhadronic_${REGIONS[$j]}.xml
    
-	sframe_batch.py $FLAG config/QCD_${REGIONS[$j]}.xml
-	sframe_batch.py $FLAG config/WJetsToQQ_${REGIONS[$j]}.xml
-	sframe_batch.py $FLAG config/ZJetsToQQ_${REGIONS[$j]}.xml
-	sframe_batch.py $FLAG config/TTBar_${REGIONS[$j]}.xml
+	# sframe_batch.py $FLAG config/QCD_${REGIONS[$j]}.xml
+	# sframe_batch.py $FLAG config/WJetsToQQ_${REGIONS[$j]}.xml
+	# sframe_batch.py $FLAG config/ZJetsToQQ_${REGIONS[$j]}.xml
+	# sframe_batch.py $FLAG config/TTBar_${REGIONS[$j]}.xml
 
 	sframe_batch.py $FLAG config/Data_${REGIONS[$j]}.xml
 done
